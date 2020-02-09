a0a08e352cd82daac5907fa0aeb83a012b79758e
Dima Butemann <dbutemann@gmail.com>
2020-02-09 10:22:40 +0100

    Add initial files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# firefox-telekom-fon-login-addon
+See https://apple.stackexchange.com/a/211604 on how to disable the captive login window in macOS.
+Otherwise, the macOS captive login window needs to be dismissed every time manually to make this work.
+
+## [Hammerspoon](https://www.hammerspoon.org/) script
+
+I also use this script to access the captive login page in Firefox as soon as I see the `You must log in to this network before you can access the Internet.` message in Firefox:
+
+```lua
+-- open captive login tab in Firefox for Telekom_FON
+hs.hotkey.bind(hyper, "g", function()
+  hs.eventtap.keyStroke({'cmd'}, 't')
+  hs.eventtap.keyStrokes('detectportal.firefox.com/success.txt')
+  hs.eventtap.keyStroke({}, 'return')
+end)
+```
---
+++ main.js
@@ -0,0 +1,47 @@
+var clickedLoginTab = false;
+
+setInterval(function() {
+	startTab = document.querySelectorAll('a.tab-login-buy:nth-child(3)')[0];
+	if (startTab.classList.contains('active')) {
+		console.log('Restarting clicking login tab!');
+		clickedLoginTab = false;
+	}
+	if (!clickedLoginTab) {
+		loginTab = document.querySelectorAll('a.tab-login-hotspot:nth-child(4)')[0];
+		if (loginTab) {
+			loginTab.click();
+			// clicking the 'Eingeloggt bleiben' checkbox
+			document.querySelectorAll('.check')[0].click();
+			// clicking the 'Online gehen' button
+			document.querySelectorAll('button.dtag-button-connect:nth-child(7)')[0].click();
+		clickedLoginTab = true;
+		}
+	}
+}, 100);
+
+
+// TODO: Maybe click Ausloggen?
+/*
+setInterval(function () {
+	document.querySelectorAll('div.tab:nth-child(2) > h1:nth-child(1) > span:nth-child(1) > span:nth-child(1)')
+}, 200);
+*/
+
+
+/*
+Apparently it is not possible to close the current tab within Firefox.
+Otherwise, the code below would work!
+
+setInterval(function() {
+	var onlineIndicator = document.querySelectorAll('.online-icon');
+	var isOnline = false;
+	onlineIndicator.forEach(function(userItem) {
+		if (userTime.innerHTML === "V") {
+			isOnline = true;
+		}
+	});
+	if (isOnline) {
+		window.close();
+	}
+}, 300);
+*/
